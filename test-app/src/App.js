4414f04f427ddbb6a4a828ad33ca3161504083c5
Karel Gonzalez <karel.gonzalez@thirteen23.com>
2019-09-16 13:58:44 -0500

    Created initial component stub; created test app to demo component

---
+++ test-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TestComp from 'test-component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <TestComp />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ test-component/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class TestComp extends React.Component {
+  render() {
+    return (
+      <div>This is a test.</div>
+    );
+  }
+}
+export default TestComp;
