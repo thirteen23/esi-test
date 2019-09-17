9fe99dd3a23d2c5c6c031e2ff3bbdf9186233d32
Karel Gonzalez <karel.gonzalez@thirteen23.com>
2019-09-17 13:28:47 -0500

    Added data render and callback on click

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -2,14 +2,50 @@ import React from 'react';
 import TestComp from 'test-component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <TestComp />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            recentlyClicked: ""
+        }
+        this.onClick = this.onClick.bind(this);
+    }
+    testData = [
+        {
+            "type":"Criteria not met",
+            "count":"1,985",
+            "percent":"44",
+            "param":"Criteria-Not-Met"
+        },
+        {
+            "type":"Adherent",
+            "count":"500",
+            "percent":"11",
+            "param":"Adherent"
+        },
+        {
+            "type":"Non-Adherent",
+            "count":"2,000",
+            "percent":"45",
+            "param":"Non-Adherent"
+        }
+    ];
+
+    onClick(from) {
+        this.setState({recentlyClicked: from})
+    }
+
+    render() {
+        return (
+            <div className = "App" >
+                <header className = "App-header" >
+                    <TestComp data={this.testData} onClick={this.onClick} />
+                    Recently clicked: {this.state.recentlyClicked}
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
--- test-component/src/index.js
+++ test-component/src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 class TestComp extends React.Component {
-  render() {
-    return (
-      <div>This is a test.</div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.propRender = this.propRender.bind(this);
+    }
+    handleClick(e, d) {
+        this.props.onClick(d)
+    }
+
+    propRender(data) {
+        return data.data.map((datum, i) => {
+            return(
+                <div key={i}
+                className='datum'
+                onClick={(e) => this.handleClick(e, datum.type)}>
+                    <div>Type: {datum.type}</div>
+                    <div>Count: {datum.count}</div>
+                    <div>Percent: {datum.percent}</div>
+                    <div>Parameter: {datum.param}</div>
+                </div>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <this.propRender data={this.props.data} />
+            </div>
+        );
+    }
 }
 export default TestComp;
