aec90d867674a135d33dd24b5e9eb922a367ba9b
Karel Gonzalez <karel.gonzalez@thirteen23.com>
2019-09-27 10:06:25 -0500

    build script update

--- test-app/start.sh
+++ test-app/start.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 pushd ../circlepack
+echo "Installing CirclePack Component"
 npm i
+echo "Building CirclePack Component"
 npm run build
+echo "Linking CirclePack Component"
 npm link
 popd
 
+echo "Installing Test App"
 npm i
+echo "Linking CirclePack Component to Test App"
 npm link circlepack
+echo "Building Test App"
 npm run build
